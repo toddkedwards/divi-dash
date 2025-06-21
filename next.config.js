48b7209d65a6226205d767618716f8913e6e0c3a
Todd K Edwards <toddkedwards@gmail.com>
2025-06-21 15:32:36 -0400

    Add external image domains to Next.js config
    - Configure Clearbit and Finnhub domains for company logos
    - Enable Next.js Image component to load external logo images
    - Supports logo.clearbit.com and finnhub.io domains

--- next.config.js
+++ next.config.js
@@ -6,6 +6,40 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['@divly/core', '@divly/data-models'],
+  images: {
+    domains: [
+      'logo.clearbit.com',
+      'finnhub.io',
+      'static.finnhub.io',
+      'assets.finnhub.io'
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'logo.clearbit.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'finnhub.io',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.finnhub.io',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.finnhub.io',
+        port: '',
+        pathname: '/**',
+      }
+    ]
+  }
 };
 
 module.exports = withBundleAnalyzer(nextConfig);
